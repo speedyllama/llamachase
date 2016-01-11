0a42dee06b111fac8a97f2832cb9436f38e8d4e1
Mike Zou <mzou@speedyllama.com>
2016-01-11 09:10:44 -0500

    i18n WIP: separate lines

--- src/util/I18n.js
+++ src/util/I18n.js
@@ -1,9 +1,15 @@
 (function(){
     var I18N_PATH = 'res/i18n.txt';
     var I18n = cc.Class.extend({
+        map: null,
+
         ctor: function() {
             var text = cc.loader.getRes(I18N_PATH);
-            console.log('####' + text);
+            var lines = text.split('\n');
+            for (var lineNum = 0; lineNum < lines.length; lineNum++) {
+                var line = lines[lineNum];
+                console.log(lineNum + ' : ' + line);
+            }
         }
     });
 
