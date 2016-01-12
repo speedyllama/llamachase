8e4a9febc8eac5ab91dec8b4d5dd0bfed81b92dc
Mike Zou <mzou@speedyllama.com>
2016-01-12 07:43:24 -0500

    i18n getText

--- src/util/I18n.js
+++ src/util/I18n.js
@@ -6,23 +6,38 @@
         ctor: function() {
             var text = cc.loader.getRes(I18N_PATH);
             var lines = text.split('\n');
-            var languageKeys;
+            var langKeys;
             var map = {};
             for (var lineNum = 0; lineNum < lines.length; lineNum++) {
                 var line = lines[lineNum];
                 var keys = line.split('\t');
                 if (lineNum == 0) {
-                    languageKeys = keys;
-                    for (var languageIndex = 1; languageIndex < languageKeys.length; languageIndex++) {
-                        map[languageKeys[languageIndex]] = {};
+                    langKeys = keys;
+                    for (var langIndex = 1; langIndex < langKeys.length; langIndex++) {
+                        map[langKeys[langIndex]] = {};
                     }
                 } else {
-                    for (var languageIndex = 1; languageIndex < languageKeys.length; languageIndex++) {
-                        map[languageKeys[languageIndex]][keys[0]] = keys[languageIndex];
+                    for (var langIndex = 1; langIndex < langKeys.length; langIndex++) {
+                        map[langKeys[langIndex]][keys[0]] = keys[langIndex];
                     }
                 }
             }
             this.map = map;
+        },
+
+        getText: function(lang, key) {
+            var val;
+            try {
+                val = this.map[lang][key];
+            } catch (e) {
+                val = undefined;
+            }
+
+            if (val == undefined) {
+                return key;
+            } else {
+                return val;
+            }
         }
     });
 
