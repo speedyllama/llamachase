873a33fb69f31bc4f3073396d8449e29579d9237
Mike Zou <mzou@speedyllama.com>
2016-01-12 08:15:19 -0500

    Add default language

--- src/util/const.js
+++ src/util/const.js
@@ -1,1 +1,2 @@
+const LANG = 'en_us';
 const I18N_PATH = 'res/i18n.txt';
--- src/util/ladybug.js
+++ src/util/ladybug.js
@@ -3,6 +3,6 @@ ladybug = {
     util: {},
 
     init: function() {
-        ladybug.util.i18n = new ladybug.util.I18n(cc.loader.getRes(I18N_PATH));
+        ladybug.util.i18n = new ladybug.util.I18n(cc.loader.getRes(I18N_PATH), LANG);
     }
 };
