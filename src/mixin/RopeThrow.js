cad36f687601727dc6f6ca8b82b2edc056aafca3
Mike Zou <mzou@speedyllama.com>
2016-01-14 08:23:49 -0500

    Rope mixin WIP

---
+++ src/mixin/RopeThrow.js
@@ -0,0 +1,59 @@
+(function(){
+    RopeThrow = cc.Class.extend({
+        layer: null,
+
+        ctor: function(layer) {
+            this.layer = layer;
+
+            var rope = new cc.Sprite('res/rope.png');
+            this.rope = rope;
+            rope.x = 512;
+            rope.y = 512;
+            this.layer.addChild(rope);
+
+            if ('touches' in cc.sys.capabilities) {
+                cc.eventManager.addListener({
+                    event: cc.EventListener.TOUCH_ONE_BY_ONE,
+                    onTouchBegan: this.onTouchBegan.bind(this),
+                    onTouchEnded: this.onTouchEnded.bind(this)
+                }, layer);
+            } else if ('mouse' in cc.sys.capabilities) {
+                cc.eventManager.addListener({
+                    event: cc.EventListener.MOUSE,
+                    onMouseDown: this.onTouchBegan.bind(this),
+                    onMouseUp: this.onTouchEnded.bind(this)
+                }, layer);
+            }
+        },
+
+        beginPos: null,
+        endPos: null,
+        rope: null,
+        ropeMoving: false,
+
+        onTouchBegan: function(touch) {
+            if (this.ropeMoving) {
+                return false;
+            }
+            this.beginPos = touch.getLocation();
+            return true;
+        },
+
+        onTouchEnded: function(touch) {
+            if (this.ropeMoving) {
+                return false;
+            }
+            this.endPos = touch.getLocation();
+            var line = new cc.DrawNode();
+            line.drawSegment(this.beginPos, this.endPos, 10, new cc.Color(0, 255, 0));
+            this.layer.addChild(line);
+            return true;
+        },
+
+        moveRope: function() {
+            this.ropeMoving = true;
+            var action = cc.moveTo(this.endPos);
+
+        }
+    });
+})();
