aa3f4a689ef142e3674987317ca45e8dcb252ebf
Mike Zou <mzou@speedyllama.com>
2016-01-21 18:52:50 -0500

    Llama is jumping

--- src/scene/HelpScene.js
+++ src/scene/HelpScene.js
@@ -8,6 +8,21 @@
             this.addChild(BackgroundFactory.newYellowBackground());
             this.ropeThrow = new RopeThrow(this);
             this.ropeThrow.moveRope(new cc.Point(300, 1000));
+
+            var llama = new cc.Sprite(res.SpeedyLlama);
+            llama.setScale(0.5, 0.5);
+            llama.x = -512;
+            llama.y = 1600;
+
+            var move = cc.moveTo(0.5, 540, 1600);
+            var jump = cc.repeatForever(cc.sequence(
+                cc.rotateTo(0.3, -15, 0),
+                cc.rotateTo(0.3, 15, 0)
+            ));
+
+            this.addChild(llama);
+            llama.runAction(move);
+            llama.runAction(jump);
         }
     });
 })();
