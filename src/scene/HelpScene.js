c6bf23b65fbaf355c86596ff01e766b2fe732028
Mike Zou <mzou@speedyllama.com>
2016-01-26 08:46:57 -0500

    Step one of help done

--- src/scene/HelpScene.js
+++ src/scene/HelpScene.js
@@ -33,10 +33,28 @@
                     button: i18n.getText('Watch Demo'),
                     callback: function(){
                         var finger = new cc.Sprite(res.Finger);
-                        finger.setPosition(512, 512);
+                        finger.setScale(0.5);
+                        finger.setPosition(600, 200);
                         this.addChild(finger);
                         this.slider.hide();
-                        setTimeout(function(){this.slider.next()}.bind(this), 3000);
+
+                        var fingerAction = cc.sequence(
+                            cc.delayTime(1),
+                            cc.moveTo(0.3, 600, 930),
+                            cc.callFunc(function(){
+                                this.ropeThrow.enable();
+                                this.ropeThrow.moveRope(new cc.Point(540, 930));
+
+                                this.runAction(cc.sequence(
+                                    cc.delayTime(3),
+                                    cc.callFunc(function(){
+                                        this.removeChild(finger);
+                                        this.slider.next();
+                                    }, this)
+                                ));
+                            }, this)
+                        )
+                        finger.runAction(fingerAction);
                     }.bind(this)
                 },{
                     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultrices mattis viverra. Morbi commodo neque nec facilisis euismod. '
